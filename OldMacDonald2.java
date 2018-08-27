674f65f5b3425ca5a785aad9b69d88986e6304cb
River Sneed <microbacondonut@gmail.com>
2018-08-27 12:40:58 -0400

    Add files via upload

--- OldMacDonald2.java
+++ OldMacDonald2.java
@@ -11,26 +11,41 @@
 public class OldMacDonald2 
 {
     
+    /**make the first line of the verse when eieio() is called
+     * 
+     */
     public static void eieio() 
     { 
         System.out.println("Old MacDonald had a farm, E I E I O.");
     }
 
 
+    /** param animal animal on the farm
+     * pusts animal into second line of the verse
+     * 
+     */
     public static void hadA(String animal) 
     { 
         System.out.println("And on that farm he had a " + animal + ", E I E I O.");
     }
 
 
-    public static void withA(String sound) 
+    /** param sound animal sound
+     * puts animal sound into third and fourth line of verse
+     * 
+     */public static void withA(String sound) 
     { 
         System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
         System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
     }
 
     
-    public static void singIt(String animal, String sound) 
+    /** param animal animal on the farm
+     * param sound animal sound
+     * passes strings into verse functions and calls them in order
+     * prints a blank line between each verse
+     * 
+     */public static void singIt(String animal, String sound) 
     {
         eieio();
         hadA(animal);
@@ -40,9 +55,12 @@ public class OldMacDonald2
     }
 
     
-    public static void main(String[] args) 
+    /**titles the song
+     * makes strings with animals and their sounds to be played latrer sk83r
+     * 
+     */public static void main(String[] args) 
     {
-        System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
+        System.out.println("Old MacDonald Had A Farm\n"); // Give our song a title
 
         singIt("duck", "quack");
         singIt("cow", "moo");
